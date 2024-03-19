57c2f7b6f01e6acfd43a8f36621332991f30b3c3
BilakshanP <pbilakshan@gmail.com>
2024-03-19 17:53:58 +0530

    reformat for static

--- tributepage/settings.py
+++ tributepage/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tributeapp.apps.TributeappConfig',
+    "tributeapp",
 ]
 
 MIDDLEWARE = [
@@ -122,9 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
