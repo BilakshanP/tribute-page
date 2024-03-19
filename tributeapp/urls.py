a0692b527d6d85047ade2b3aa848fbc238126d6f
BilakshanP <pbilakshan@gmail.com>
2024-03-19 16:02:06 +0530

    index refcators etc

--- tributeapp/urls.py
+++ tributeapp/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.root, name='root'),
     # path('route/a/', views.route_a, name='route_a'),
     # path('route/b/', views.route_b, name='route_b'),
     # path('example/', views.example_view, name='example'),
     # path('test/', views.test, name='test'),
     path('profile/', views.profile, name='profile'),
 ]
+
+root = [path('', views.root, name='root')]
