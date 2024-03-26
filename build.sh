0f697795337b4be048e21d12ae58e74d35c98880
BilakshanP <pbilakshan@gmail.com>
2024-03-26 11:56:17 +0530

    geez

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 echo STARTING BUILD
 python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic # --noinput # --clear
+python3 manage.py collectstatic --noinput # --clear
 echo FINISHED BUILD
